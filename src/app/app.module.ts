7ecf6d257705aedb5afdd9b613cbac2b324bcaba
wssds Sn Gk <ssean.gerigk@gmx.net>
2022-01-14 11:37:25 +0100

    more work on my personal portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import {FirstSectionComponent} from './first-section/first-section.component'
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FirstSectionComponent,
+    MySkillsComponent,
+    MyProjectsComponent,
+    AboutMeComponent,
+    ContactMeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
