0f5d5a8b8e5cc44a14142eeb1eaccccf3db9c5de
wssds Sn Gk <ssean.gerigk@gmx.net>
2022-01-25 14:07:56 +0100

    done

--- src/app/in-view.directive.spec.ts
+++ src/app/in-view.directive.spec.ts
@@ -1,65 +1,65 @@
-import { AfterViewInit, OnDestroy } from '@angular/core';
-import { InViewDirective } from './in-view.directive';
+// import { AfterViewInit, OnDestroy } from '@angular/core';
+// import { InViewDirective } from './in-view.directive';
 
-// describe('InViewDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new InViewDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
+// // describe('InViewDirective', () => {
+// //   it('should create an instance', () => {
+// //     const directive = new InViewDirective();
+// //     expect(directive).toBeTruthy();
+// //   });
+// // });
 
-/**
- * The view state of the element been observed.
- */
- declare interface VisibilityState {
-  elem: ElementRef;
-  view: 'VISIBLE' | 'HIDDEN';
-}
+// /**
+//  * The view state of the element been observed.
+//  */
+//  declare interface VisibilityState {
+//   elem: ElementRef;
+//   view: 'VISIBLE' | 'HIDDEN';
+// }
 
-@Directive({
-  selector: '[appInView]',
-})
-export class InViewDirective implements AfterViewInit, OnDestroy {
-  /**
-   * Emited each time @method _callback has been called
-   */
-  @Output() visibilityChange = new EventEmitter<VisibilityState>();
+// @Directive({
+//   selector: '[appInView]',
+// })
+// export class InViewDirective implements AfterViewInit, OnDestroy {
+//   /**
+//    * Emited each time @method _callback has been called
+//    */
+//   @Output() visibilityChange = new EventEmitter<VisibilityState>();
 
-  /**
-   * @see Documentation https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
-   */
-  private _observer: IntersectionObserver;
+//   /**
+//    * @see Documentation https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+//    */
+//   private _observer: IntersectionObserver;
 
-  constructor(private _elementRef: ElementRef) {}
+//   constructor(private _elementRef: ElementRef) {}
 
-  ngAfterViewInit(): void {
-    const options = { root: null, rootMargin: '0px', threshold: 1.0 };
-    this._observer = new IntersectionObserver(this._callback, options);
-    this._observer.observe(this._elementRef.nativeElement);
-  }
+//   ngAfterViewInit(): void {
+//     const options = { root: null, rootMargin: '0px', threshold: 1.0 };
+//     this._observer = new IntersectionObserver(this._callback, options);
+//     this._observer.observe(this._elementRef.nativeElement);
+//   }
 
-  /**
-   * Callback Function every time the observed element changes visibility state.
-   * Visibility state depends on the threshold values of the observer
-   *
-   * @see Documentation https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
-   */
-  private _callback: IntersectionObserverCallback = (entries, observer) => {
-    entries.forEach((entry) =>
-      this.visibilityChange.emit(this._getVisibilityState(entry))
-    );
-  };
+//   /**
+//    * Callback Function every time the observed element changes visibility state.
+//    * Visibility state depends on the threshold values of the observer
+//    *
+//    * @see Documentation https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+//    */
+//   private _callback: IntersectionObserverCallback = (entries, observer) => {
+//     entries.forEach((entry) =>
+//       this.visibilityChange.emit(this._getVisibilityState(entry))
+//     );
+//   };
 
-  /**
-   * Gives back the Visibility State of the element been observed.
-   */
-  _getVisibilityState(entry: IntersectionObserverEntry): VisibilityState {
-    console.log(entry);
-    let result = entry.isIntersecting ? 'VISIBLE' : 'HIDDEN';
-    return { elem: this._elementRef, view: result } as VisibilityState;
-  }
+//   /**
+//    * Gives back the Visibility State of the element been observed.
+//    */
+//   _getVisibilityState(entry: IntersectionObserverEntry): VisibilityState {
+//     console.log(entry);
+//     let result = entry.isIntersecting ? 'VISIBLE' : 'HIDDEN';
+//     return { elem: this._elementRef, view: result } as VisibilityState;
+//   }
 
-  ngOnDestroy(): void {
-    this._observer.disconnect();
-  }
-}
+//   ngOnDestroy(): void {
+//     this._observer.disconnect();
+//   }
+// }
